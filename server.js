a3edec2a7440b840b3f2f2c2309228b41b3ca882
Claudio Puggioni <claudieddu@hotmail.com>
2022-09-09 00:24:24 -0700

    why is it crashing

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.static("public"));
 
-const httpServer = app.listen(process.env.PORT || 8371 || 3333);
+const httpServer = app.listen(parseInt(process.env.PORT) || 3000 || 8371 || 3333);
 
 const io = new Server(httpServer, {
   cors: {
